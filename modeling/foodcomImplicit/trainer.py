c387117c911944c65ea9f49622edb3c117dfea50
SNMHZ <snmhz325@gmail.com>
2022-05-24 06:40:02 +0000

    feat: Update ALS GPU option

--- modeling/foodcomImplicit/trainer.py
+++ modeling/foodcomImplicit/trainer.py
@@ -1,7 +1,9 @@
+import os
 import gc
+
 import torch
 
-import scipy
+from  scipy.sparse import csr_matrix
 import numpy as np
 
 import implicit
@@ -11,25 +13,39 @@ def get_model(args):
     """
     Load model and move tensors to a given devices.
     """
+    isGPU = False
+    if args.device == "cuda":
+        isGPU = True
+    else:
+        os.popen("export OPENBLAS_NUM_THREADS=1")
+
+    print('isGPU: ', isGPU)
 
     if args.model == "als":
         model = implicit.als.AlternatingLeastSquares(factors=args.factors, 
-                regularization=args.regularization, iterations=1, 
-                random_state=args.seed, use_gpu=False)
+                regularization=args.regularization, iterations=2, 
+                random_state=args.seed, use_gpu=isGPU)
 
     return model
 
 def validate(model, valid_data, args) -> float:
-    valid_cut = valid_data[:model.user_factors.shape[0], :model.item_factors.shape[0]]
+    if isinstance(model.user_factors, np.ndarray):
+        user_factors = model.user_factors
+        item_factors = model.item_factors
+    else:
+        user_factors = model.user_factors.to_numpy()
+        item_factors = model.item_factors.to_numpy()
+    
+    valid_cut = valid_data[:user_factors.shape[0], :item_factors.shape[0]]
     
     valid_data.nonzero()
-    valid_prefrence = (model.user_factors[valid_cut.nonzero()[0]] * model.item_factors[valid_cut.nonzero()[1]]).sum(axis=1)
+    valid_prefrence = (user_factors[valid_cut.nonzero()[0]] * item_factors[valid_cut.nonzero()[1]]).sum(axis=1)
     loss = np.abs(valid_cut.data-valid_prefrence).sum()/valid_data.data.size
 
 
     return loss
 
-def run(args, train_data : scipy.sparse.csr_matrix, valid_data : scipy.sparse.csr_matrix):
+def run(args, train_data : csr_matrix, valid_data : csr_matrix):
     torch.cuda.empty_cache()
     gc.collect()
 
@@ -43,8 +59,14 @@ def run(args, train_data : scipy.sparse.csr_matrix, valid_data : scipy.sparse.cs
         if cur_loss < best_loss:
             best_loss = cur_loss
             early_stopping_counter = 0
-            np.save("_als_userfactors.npy", model.user_factors)
-            np.save("_als_itemfactors.npy", model.item_factors)
+            if isinstance(model.user_factors, np.ndarray):
+                user_factors = model.user_factors
+                item_factors = model.item_factors
+            else:
+                user_factors = model.user_factors.to_numpy()
+                item_factors = model.item_factors.to_numpy()
+            np.save("_als_userfactors.npy", user_factors)
+            np.save("_als_itemfactors.npy", item_factors)
         else:
             early_stopping_counter += 1
             if early_stopping_counter >= args.patience:
