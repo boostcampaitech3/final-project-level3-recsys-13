3b6e84f58a85fbc541388cdcc9f460914f6e0862
SNMHZ <snmhz325@gmail.com>
2022-05-24 06:40:32 +0000

    feat: Update ALS modeling `train by datasets`

--- modeling/foodcomImplicit/dataloader.py
+++ modeling/foodcomImplicit/dataloader.py
@@ -1,7 +1,9 @@
 import os
-import scipy
+
 import numpy as np
 import pandas as pd
+
+from scipy.sparse import csr_matrix
 
 def read_data(args):
     train_file_path = os.path.join(args.data_dir, "interactions_train.csv")
@@ -12,14 +14,22 @@ def read_data(args):
     df_valid = pd.read_csv(valid_file_path)
     df_test = pd.read_csv(test_file_path)
 
+    if args.data_to_feed == "tr":
+        pass
+    elif args.data_to_feed == "trval":
+        df_train = pd.concat([df_train, df_valid])
+    elif args.data_to_feed == "all":
+        df_train = pd.concat([df_train, df_valid, df_test])
+    
     train_data = get_csr_matrix(df_train)
     valid_data = get_csr_matrix(df_valid)
     test_data = get_csr_matrix(df_test)
 
+
     return train_data, valid_data, test_data
 
-def get_csr_matrix(df : pd.DataFrame, use_rating=False) -> scipy.sparse.csr_matrix:
+def get_csr_matrix(df : pd.DataFrame, use_rating=False) -> csr_matrix:
     _view = [1]*df.shape[0] if not use_rating else df['rating'].values
-    _matrix = scipy.sparse.csr_matrix((_view, (df['u'], df['i'])), \
-                                        shape=(df['u'].max()+1, df['i'].max()+1))
+    _matrix = csr_matrix((_view, (df['u'], df['i'])), \
+                        shape=(df['u'].max()+1, df['i'].max()+1))
     return _matrix
