056cf5b4d19fa78b399604345253b65c436d8277
SNMHZ <snmhz325@gmail.com>
2022-06-02 08:02:07 +0000

    feat: Update webhook url

--- cicd/build_webhook.py
+++ cicd/build_webhook.py
@@ -6,18 +6,16 @@ import requests
 
 app = FastAPI(title="build_webhook", debug=False, version="0.1.0")
 
-class GeneralRequest(BaseModel):
-    data : str
 
 @app.post("/api/v1/build_webhook", description="빌드 시작 트리거")
-async def build_webhook(general: GeneralRequest):
+async def build_webhook(general):
     try:
         branch_name = general.data
         subprocess.call(f"git checkout {branch_name}", shell=True)
         subprocess.call("git pull", shell=True)
         subprocess.call("chmod 777 ./build.sh", shell=True)
         result = subprocess.run(["./build.sh"], stdout=subprocess.PIPE, text=True, shell=True)
-        requests.post("http://34.64.137.90:30003/api/v1/deploy_webhook", json={"data":'go'}).json()
+        requests.post("http://115.85.183.238:30003/api/v1/deploy_webhook", json={"data":'go'}).json()
         print(result.stdout)
         print(branch_name)
         return {"result": "success"}
