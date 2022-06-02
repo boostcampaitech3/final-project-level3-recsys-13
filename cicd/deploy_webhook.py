3cf754a3b47afb056003bb77c249e827774ce13f
SNMHZ <snmhz325@gmail.com>
2022-06-02 15:13:34 +0000

    feat: Update req dest after firewall setting

--- cicd/deploy_webhook.py
+++ cicd/deploy_webhook.py
@@ -1,24 +1,44 @@
-import uvicorn
-from fastapi import FastAPI
-from pydantic import BaseModel
+# import uvicorn
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# import subprocess
+
+# app = FastAPI(title="deploy_webhook", debug=False, version="0.1.0")
+
+# class GeneralRequest(BaseModel):
+#     data : str
+
+# @app.post("/api/v1/deploy_webhook", description="배포 시작 트리거")
+# async def deploy_webhook(general: GeneralRequest):
+#     try:
+#         subprocess.call("chmod 777 ./deploy.sh", shell=True)
+#         result = subprocess.run(["./deploy.sh"], stdout=subprocess.PIPE, text=True, shell=True)
+#         print(result.stdout)
+#         print(general.data)
+#         return {"result": "success"}
+#     except Exception as e:
+#         print(e)
+#         return {"result": "fail"}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=30003, reload=False, debug=True)
+
+from flask import Flask
 import subprocess
 
-app = FastAPI(title="deploy_webhook", debug=False, version="0.1.0")
+app = Flask(__name__)
 
-class GeneralRequest(BaseModel):
-    data : str
-
-@app.post("/api/v1/deploy_webhook", description="배포 시작 트리거")
-async def deploy_webhook(general: GeneralRequest):
+@app.route("/api/v1/deploy_webhook", methods=['POST'])
+def deploy_webhook():
     try:
         subprocess.call("chmod 777 ./deploy.sh", shell=True)
         result = subprocess.run(["./deploy.sh"], stdout=subprocess.PIPE, text=True, shell=True)
         print(result.stdout)
-        print(general.data)
         return {"result": "success"}
     except Exception as e:
         print(e)
         return {"result": "fail"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=30003, reload=False, debug=True)
+    app.run(host='0.0.0.0', port=30003, debug=True)
+    #app.run(debug=True)
