8edceff32e9b8f86757f534123bbfee2a11faf26
SNMHZ <snmhz325@gmail.com>
2022-06-02 16:17:12 +0000

    test: cd test 4

--- frontend/signinPage.py
+++ frontend/signinPage.py
@@ -40,7 +40,7 @@ def login():
         st.experimental_rerun()
 
 def signinPage():
-    st.title("Login - CD test ! 3")
+    st.title("Login - CD test ! 4")
     st.session_state.user_id = -1
     st.title(st.session_state.user_id)
     st.session_state.id = st.text_input('Id')
