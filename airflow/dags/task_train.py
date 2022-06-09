84e9f06bb5d64269d8fd519a57115687f0d48458
김원섭 <whattsup_kim@naver.com>
2022-06-09 17:02:55 +0900

    fix: cron expression

--- airflow/dags/task_train.py
+++ airflow/dags/task_train.py
@@ -13,100 +13,91 @@ import requests
 
 models = ["BPR", "LightGCN", "MultiVAE", "MultiDAE", "CDAE", "RecVAE"]
 
-
 def get_db_engine():
-    """Returns a connection and a metadata object"""
+    '''Returns a connection and a metadata object'''
     engine = sqlalchemy.create_engine(DATABASE_URL, echo=True)
-    # meta = sqlalchemy.MetaData(bind=engine, reflect=True)
+    #meta = sqlalchemy.MetaData(bind=engine, reflect=True)
     return engine  # , meta
 
-
 tasks_train = []
-# local_tz = pendulum.timezone("Asia/Seoul")
+#local_tz = pendulum.timezone("Asia/Seoul")
 
 default_args = {
-    "owner": "jinsu",
-    "depends_on_past": False,  # 이전 DAG의 Task가 성공, 실패 여부에 따라 현재 DAG 실행 여부가 결정됨. False는 과거의 실행 결과 상관없이 매일 실행
+    'owner' : 'jinsu',
+    'depends_on_past' : False, # 이전 DAG의 Task가 성공, 실패 여부에 따라 현재 DAG 실행 여부가 결정됨. False는 과거의 실행 결과 상관없이 매일 실행
     #'start_date' : datetime(2022, 6, 7, tzinfo=local_tz),
-    "start_date": datetime(2022, 6, 7),
-    "retries": 3,  # 실패시 재시도 횟수
-    "retry_delay": timedelta(minutes=1),  # 만약 실패시 1분 뒤 재실행
+    'start_date' : datetime(2022, 6, 7),
+    'retries' : 3, # 실패시 재시도 횟수
+    "retry_delay" : timedelta(minutes = 1) #만약 실패시 1분 뒤 재실행
 }
 
-
 def confirm_data():
-    # 0 이면 not change 1이면 train
+    #0 이면 not change 1이면 train
     print("confirm_data")
     change_branchs = ["not_change", "batch_tag"]
-
-    with open("info.yaml") as f:
+    
+    with open('info.yaml') as f:
         info = yaml.load(f, Loader=yaml.FullLoader)
-
+    
     engine = get_db_engine()
     meta = pd.read_sql(f"select * from public.meta_data", engine)
-    db_interaction_n = meta["interaction_count"].item()
+    db_interaction_n = meta['interaction_count'].item()
 
     flag = 1 if info["now_interaction_count"] < db_interaction_n else 0
-
-    info["now_interaction_count"] = db_interaction_n
-    with open("info.yaml", "w") as f:
+    
+    info['now_interaction_count'] = db_interaction_n
+    with open('info.yaml', 'w') as f:
         yaml.dump(info, f)
-
+        
     return change_branchs[flag]
-
 
 def not_change():
     print("notchange:notchange")
 
-
 def train_start(**context):
     # Train하기
-    tmp = (
-        requests.post(
-            f"http://118.67.132.123:30001/modeling",
-            json={"name": context["params"]["name"], "config": {}},
-        ).json(),
-    )
-
+    tmp = requests.post(f"http://118.67.132.123:30001/modeling", json= {"name":context["params"]["name"], "config":{}}).json(),
 
 # with 구문으로 DAG 정의
 with DAG(
-    dag_id="train_start",
+    dag_id='train_start',
     default_args=default_args,
-    schedule_interval="0 0 0/4 * *",
-    tags=["boostcamp_ai_final"],
-) as dag:
+    schedule_interval = '0 0/4 * * *',
+    tags=['boostcamp_ai_final']
+)as dag:
 
     task_confirm_data = BranchPythonOperator(
-        task_id="confirm_data", python_callable=confirm_data
+        task_id = "confirm_data",
+        python_callable = confirm_data
     )
 
     task_batch_tag = PythonOperator(
         task_id="batch_tag",
-        python_callable=train_start,
-        params={"name": "--update_batch_tag"},
-        provide_context=True,
+        python_callable = train_start,
+        params={"name":"--update_batch_tag"},
+        provide_context=True
     )
 
-    task_not_change = PythonOperator(task_id="not_change", python_callable=not_change)
+    task_not_change = PythonOperator(
+        task_id="not_change",
+        python_callable = not_change
+    )
 
     task_complete = PythonOperator(
         task_id="complete",
-        python_callable=train_start,
-        params={"name": "--inference_info"},
-        provide_context=True,
+        python_callable = train_start,
+        params={"name":"--inference_info"},
+        provide_context=True
     )
 
     for i in range(len(models)):
-        tasks_train.append(
-            PythonOperator(
-                task_id=f"model{i+1}",
-                python_callable=train_start,
-                params={"name": models[i]},
-                provide_context=True,
-            )
-        )
+        tasks_train.append(PythonOperator(
+            task_id=f"model{i+1}",
+            python_callable = train_start,
+            params={"name":models[i]},
+            provide_context=True
+        ))
 
     task_confirm_data >> [task_batch_tag, task_not_change]
     task_batch_tag >> tasks_train >> task_complete
-
+    
